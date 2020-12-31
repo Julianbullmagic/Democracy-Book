514309112909b06f70f6e24488a7874e48642ec5
julianbullmagic <julianbullmagic@gmail.com>
2020-12-31 15:30:15 +1100

    ssssss

--- server.js
+++ server.js
@@ -3,10 +3,37 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors  = require('cors')
 const path = require('path')
-// const express = require( 'express')
-// const path = require( 'path')
-// const bodyParser = require( 'body-parser')
-// const cookieParser = require( 'cookie-parser')
+
+const routes = require('./routes')
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
+mongoose
+  .connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/db_collection", { useNewUrlParser: true })
+  .then(() => console.log("connected to mongodb"))
+  .catch(err => console.log(err));
+app.use('/',routes)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname,'client','build','index.html'))
+    })
+}
+
+app.listen(PORT, () => console.log(`server is up at ${PORT}`));
+
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const cors  = require('cors')
+// const path = require('path')
 // const compress = require( 'compression')
 // const cors = require( 'cors')
 // const helmet = require( 'helmet')
@@ -28,25 +55,25 @@ const path = require('path')
 // //comment out before building for production
 // const devBundle = require( './devBundle')
 // const routes = require('./routes')
-const PORT = process.env.PORT || 5000
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors())
-mongoose
-  .connect(process.env.MONGOLAB_URI || "mongodb+srv://Julian_Bull:bIGP1SxlM3RvYHEl@cluster0.k6i5j.mongodb.net/Democracy-Book?retryWrites=true&w=majority", { useNewUrlParser: true })
-  .then(() => console.log("connected to mongodb"))
-  .catch(err => console.log(err));
-app.use('/',routes)
-
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-
-    app.get('*',(req,res) => {
-        res.sendFile(path.join(__dirname,'client','build','index.html'))
-    })
-}
-
-app.listen(PORT, () => console.log(`server is up at ${PORT}`));
+// const PORT = process.env.PORT || 5000
+//
+// const app = express();
+//
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(cors())
+// mongoose
+//   .connect(process.env.MONGOLAB_URI || "mongodb+srv://Julian_Bull:bIGP1SxlM3RvYHEl@cluster0.k6i5j.mongodb.net/Democracy-Book?retryWrites=true&w=majority", { useNewUrlParser: true })
+//   .then(() => console.log("connected to mongodb"))
+//   .catch(err => console.log(err));
+// app.use('/',routes)
+//
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
+//
+//     app.get('*',(req,res) => {
+//         res.sendFile(path.join(__dirname,'client','build','index.html'))
+//     })
+// }
+//
+// app.listen(PORT, () => console.log(`server is up at ${PORT}`));
