eb5264e720b2e0be2244f0f48165f00837bc2315
julianbullmagic <julianbullmagic@gmail.com>
2020-12-31 15:27:36 +1100

    ddddddd

--- server.js
+++ server.js
@@ -3,36 +3,31 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors  = require('cors')
 const path = require('path')
-const express = require( 'express')
-const path = require( 'path')
-const bodyParser = require( 'body-parser')
-const cookieParser = require( 'cookie-parser')
-const compress = require( 'compression')
-const cors = require( 'cors')
-const helmet = require( 'helmet')
-const Template = require( './template')
-const userRoutes = require( './routes/user.routes')
-const authRoutes = require( './routes/auth.routes')
-const postRoutes = require( './routes/post.routes')
-const groupsRoutes = require( './routes/groups.routes')
-const rulesRoutes = require( './routes/rules.routes')
-const expertRoutes = require( './routes/expert.routes')
-const marketplaceRoutes = require( './routes/marketplace.routes')
-const React = require( 'react')
-const ReactDOMServer = require( 'react-dom/server')
-const MainRouter = require( './client/MainRouter')
-const { StaticRouter } = require( 'react-router-dom')
-
-const { ServerStyleSheets } = require( '@material-ui/styles')(ServerStyleSheets)
-const { ThemeProvider } = require( '@material-ui/styles')(ThemeProvider)
-
-const theme = require( './client/theme')
-//end
-
-//comment out before building for production
-const devBundle = require( './devBundle')
-
-const routes = require('./routes')
+// const express = require( 'express')
+// const path = require( 'path')
+// const bodyParser = require( 'body-parser')
+// const cookieParser = require( 'cookie-parser')
+// const compress = require( 'compression')
+// const cors = require( 'cors')
+// const helmet = require( 'helmet')
+// const Template = require( './template')
+// const userRoutes = require( './routes/user.routes')
+// const authRoutes = require( './routes/auth.routes')
+// const postRoutes = require( './routes/post.routes')
+// const groupsRoutes = require( './routes/groups.routes')
+// const rulesRoutes = require( './routes/rules.routes')
+// const expertRoutes = require( './routes/expert.routes')
+// const marketplaceRoutes = require( './routes/marketplace.routes')
+// const React = require( 'react')
+// const ReactDOMServer = require( 'react-dom/server')
+// const MainRouter = require( './client/MainRouter')
+// const { StaticRouter } = require( 'react-router-dom')
+// const { ServerStyleSheets } = require( '@material-ui/styles')(ServerStyleSheets)
+// const { ThemeProvider } = require( '@material-ui/styles')(ThemeProvider)
+// const theme = require( './client/theme')
+// //comment out before building for production
+// const devBundle = require( './devBundle')
+// const routes = require('./routes')
 const PORT = process.env.PORT || 5000
 
 const app = express();
