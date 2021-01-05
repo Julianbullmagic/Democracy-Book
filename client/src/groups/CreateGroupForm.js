2964c29ca2dd472a4e09d5c491cf577602cc9263
julianbullmagic <Julianbullmagic@gmail.com>
2021-01-05 14:03:10 +1100

    ddddd

--- client/src/groups/CreateGroupForm.js
+++ client/src/groups/CreateGroupForm.js
@@ -37,7 +37,7 @@ e.preventDefault()
             "Content-type": "application/json; charset=UTF-8"}}
 
 
-      fetch(uri+"/groups/add", options)
+      fetch("/groups/add", options)
               .then(response => response.json()).then(json => console.log(json));
 setToggle(!toggle)
 
